f814f420ef81ba293d73b3c7231625a07f44ba2c
vetri1203 <vetriveljaganathan5125@gmail.com>
2023-12-28 21:01:48 +0530

    Login page

---
+++ client/src/Component/Signup.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+function Signup() {
+  return (
+    <div className='signupContainer'>
+
+      <p><Link to='/'>Login</Link></p>
+    </div>
+  )
+}
+
+export default Signup;  
